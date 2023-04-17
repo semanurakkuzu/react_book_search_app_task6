401defb4a5d41aa177a9deef5ecdc4f425c70d96
semanurakkuzu <semanurakkuzu@gmail.com>
2023-04-17 18:27:36 +0300

    added book search api and maps

--- src/components/BookSearch.js
+++ src/components/BookSearch.js
@@ -14,61 +14,96 @@ import {
   CardBody,
   Image,
   Stack,
-  Divider,
   Heading
 } from '@chakra-ui/react'
 import { Search2Icon } from '@chakra-ui/icons'
+import axios from 'axios'
+
 function BookSearch() {
- 
+  const [books, setBooks] = useState()
+  const [searchbook, setSearchBook] = useState('')
+
+  function handleChange(event) {
+    setSearchBook(event.target.value)
+  }
+
+  function formSubmit(event) {
+    event.preventDefault()
+
+    axios(`https://www.googleapis.com/books/v1/volumes?q=${searchbook}`)
+      .then((res) => setBooks(res.data.items))
+      .catch((e) => console.log(e))
+  }
+
   return (
-    <>
-      <Center h="150px" color="black">
-        <Text fontSize="6xl">Search Book</Text>
-      </Center>
-      <Container marginBottom={10}>
-        <FormControl>
+    <Box
+      w="100%"
+      h="260px"
+      backgroundImage="url('https://images.unsplash.com/photo-1481627834876-b7833e8f5570?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80')"
+    >
+      <Container marginBottom={20}>
+        <Center h="150px" color="white">
+          <Text fontSize="6xl">Search Book</Text>
+        </Center>
+        <form onSubmit={formSubmit}>
           <HStack spacing="25px">
-            <Box w="400px" h="25px">
-              <Input />
-            </Box>
+            <FormControl>
+              <Box w="400px" h="25px">
+                <Input
+                  bg="white"
+                  value={searchbook}
+                  onChange={handleChange}
+                  placeholder="book title"
+                />
+              </Box>
+            </FormControl>
             <Box>
-              <Button mt={4} colorScheme="blue" type="submit">
+              <Button mt={4} colorScheme="gray" type="submit">
                 <Search2Icon />
               </Button>
             </Box>
           </HStack>
-        </FormControl>
+        </form>
       </Container>
       <Container maxW="8xl" paddingLeft="8">
-        <Grid templateColumns="repeat(5, 1fr)" gap={6}>
-          <GridItem w="100%">
-            <Card maxW="sm">
-              <CardBody>
-                <Center>
-                  <Image
-                    src="https://img.kitapyurdu.com/v1/getImage/fn:11216058/wh:true/wi:220"
-                    alt="Green double couch with wooden legs"
-                    borderRadius="md"
-                  />
-                </Center>
-                <Stack mt="6" spacing="3">
-                  <Center>
-                    <Heading size="md">Kaygısız Beyin</Heading>
-                  </Center>
-                  <Center>
-                    <Text fontSize="sm">Dr. Catherine M. Pittman</Text>
-                  </Center>
-                  <Button variant="ghost" colorScheme="blue">
-                    Details
-                  </Button>
-                </Stack>
-              </CardBody>
-              <Divider />
-            </Card>
-          </GridItem>
+        <Grid templateColumns="repeat(4, 1fr)" gap={6}>
+          {books &&
+            books.map((book) => (
+              <GridItem w="100%" key={book.id}>
+                <Card h={360}>
+                  <CardBody>
+                    <Center>
+                      <Image
+                        h={200}
+                        w={140}
+                        src={
+                          book.volumeInfo.imageLinks?.thumbnail ||
+                          'https://media.istockphoto.com/id/1132987502/tr/vekt%C3%B6r/vekt%C3%B6r-ger%C3%A7ek%C3%A7i-bo%C5%9F-kitap-kapa%C4%9F%C4%B1-%C3%BCst-g%C3%B6r%C3%BCn%C3%BCm-yal%C4%B1t%C4%B1lm%C4%B1%C5%9F.jpg?s=170667a&w=0&k=20&c=o7q9Yrn1hoZmUPXvf9qssdoljtgz-zfkM2Z7Gz-nCzw='
+                        }
+                        alt={book.volumeInfo.title}
+                        borderRadius="md"
+                      />
+                    </Center>
+                    <Stack mt="6" spacing="3">
+                      <Center>
+                        <Heading size="xs">{book.volumeInfo.title}</Heading>
+                      </Center>
+                      <Center mt="5">
+                        <Text fontSize="xs">
+                          {book.volumeInfo.authors?.join(', ')}
+                        </Text>
+                      </Center>
+                      <Button variant="link" colorScheme="blue" fontSize="xs">
+                        Detail
+                      </Button>
+                    </Stack>
+                  </CardBody>
+                </Card>
+              </GridItem>
+            ))}
         </Grid>
       </Container>
-    </>
+    </Box>
   )
 }
 
