dbccfd64544602646d6b93e838c4e9a4ac247f6c
semanurakkuzu <semanurakkuzu@gmail.com>
2023-04-18 11:37:06 +0300

    moved chakra provider

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import * as React from 'react'
-
-// 1. import `ChakraProvider` component
-import { ChakraProvider } from '@chakra-ui/react'
 import BookSearch from './components/BookSearch';
 
 function App() {
-  // 2. Wrap ChakraProvider at the root of your app
+ 
   return (
-    <ChakraProvider>
+    <>
       <BookSearch/>
-    </ChakraProvider>
+    </>
   )
 }
 
