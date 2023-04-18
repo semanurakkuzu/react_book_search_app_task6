b3961f18e4b5d105f6fe6c0097033614573cf677
semanurakkuzu <semanurakkuzu@gmail.com>
2023-04-18 11:37:43 +0300

    added chakra provider and router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css';
 import App from './App';
+import { ChakraProvider } from '@chakra-ui/react'
+import BookDetail from './components/BookDetail';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/book/:bookId",
+    element: <BookDetail />,
+  },
+
+
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  < ChakraProvider >
+   <RouterProvider router={router} />
+  </ ChakraProvider >
 );
